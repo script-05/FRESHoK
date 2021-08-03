081b1ddfb9b9f431446385cfef8021d12ca45c58
script-05 <script-05@mail.ru>
2021-08-04 01:59:43 +0500

    added footer, slider, advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function() {
+$(function () {
+
+  $('.catalog__btn, .catalog__items').on('mouseenter mouseleave', function(){
+    $('.catalog__items').toggleClass('catalog__items--active');
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+  });
+
+  $('.slider__wrapper').slick({
+    dots: false,
+    arrows: true,
+    nextArrow: '<button type="button" class="slick__btn slick__btn--next"></button>',
+    prevArrow: '<button type="button" class="slick__btn slick__btn--prev"></button>',
+    
+  });
   
 });
